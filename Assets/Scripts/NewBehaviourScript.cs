a64d57fd98e36a2d33aa82ab8e7277259aba43c0
akaishi <galigalikun@gmail.com>
2017-07-22 06:59:06 +0900

    試しサンプル

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        aaa();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void aaa()
+    {
+        Task.Delay(100);
+    }
+}
